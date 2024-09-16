65edef253261d9a69047ade28a15676a8e5ccf10
Roman Matsypula <r.matsypula@letyshops.com>
2024-09-16 18:09:33 +0300

    remove sortPipe in app template. Use logic in construtor

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'sort',
   standalone: true,
+  pure: true,
 })
 export class SortPipe implements PipeTransform {
   transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
